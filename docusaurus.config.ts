642276b9cd445db5279aaac71cf1e97044d46fe6
fauzilxx <171055934+fauzilxx@users.noreply.github.com>
2026-03-02 17:44:51 +0700

    ubah nama web di tab

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'Praktikum Struktur Data Algoritma',
+  title: 'SDA26',
   tagline: 'Materi Pembelajaran dan Penugasan Praktikum',
   favicon: 'img/favicon.svg',
 
